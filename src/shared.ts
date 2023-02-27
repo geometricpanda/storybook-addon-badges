91d903212a52a65b9f3fdf4583d3f8045d328ff6
Jim Drury <jim.drury@virginmediao2.co.uk>
2023-02-27 10:49:23 +0000

    refactor: migrates badges into storybook addon kit format [skip ci]

---
+++ src/shared.ts
@@ -0,0 +1,71 @@
+import {BadgeConfig, BadgesConfig} from "./typings";
+
+export const PARAM_BADGES_CONFIG_KEY = 'badgesConfig';
+export const PARAM_BADGES_KEY = 'badges';
+
+export enum BADGE {
+    DEFAULT = 'default',
+    BETA = 'beta',
+    STABLE = 'stable',
+    NEEDS_REVISION = 'needs-revision',
+    OBSOLETE = 'obsolete',
+    EXPERIMENTAL = 'experimental',
+    DEPRECATED = 'deprecated',
+}
+
+export const defaultBadgesConfig: BadgesConfig = {
+    [BADGE.DEFAULT]: {
+        title: 'Badge',
+    },
+    [BADGE.BETA]: {
+        title: 'Beta',
+        styles: {
+            backgroundColor: '#D6E0FF',
+            borderColor: '#2952CC',
+            color: '#2952CC',
+        },
+    },
+    [BADGE.STABLE]: {
+        title: 'Stable',
+        styles: {
+            backgroundColor: '#DCF2EA',
+            borderColor: '#317159',
+            color: '#317159',
+        },
+    },
+    [BADGE.NEEDS_REVISION]: {
+        title: 'Needs Revision',
+        styles: {
+            backgroundColor: '#FFEFD2',
+            borderColor: '#66460D',
+            color: '#66460D',
+        },
+    },
+    [BADGE.OBSOLETE]: {
+        title: 'Obsolete',
+        styles: {
+            backgroundColor: '#F9DADA',
+            borderColor: '#7D2828',
+            color: '#7D2828',
+        },
+    },
+    [BADGE.EXPERIMENTAL]: {
+        title: 'Experimental',
+        styles: {
+            backgroundColor: '#E7E4F9',
+            borderColor: '#6E62B6',
+            color: '#6E62B6',
+        },
+    },
+    [BADGE.DEPRECATED]: {
+        title: 'Deprecated',
+        styles: {
+            backgroundColor: '#F8E3DA',
+            borderColor: '#85462B',
+            color: '#85462B',
+        },
+    },
+};
+
+export const defaultBadgeConfig: BadgeConfig =
+    defaultBadgesConfig[BADGE.DEFAULT];
