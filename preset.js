717ea065f5bb42950acde39d0c44160c8bb3849d
Jim Drury <jim.drury@virginmediao2.co.uk>
2023-02-27 11:58:55 +0000

    fix: resolves issue with preview preset

--- preset.js
+++ preset.js
@@ -1,6 +1,6 @@
-function config(entry = []) {
-  return [...entry, require.resolve("./dist/esm/preset/preview")];
-}
+// function config(entry = []) {
+//   return [...entry, require.resolve("./dist/esm/preset/preview")];
+// }
 
 function managerEntries(entry = []) {
   return [...entry, require.resolve("./dist/esm/preset/manager")];
@@ -8,5 +8,4 @@ function managerEntries(entry = []) {
 
 module.exports = {
   managerEntries,
-  config,
 };
