7bdeff5ec94ee62d22327659bd3aed40262f46f0
geometricpanda <geometricpanda@users.noreply.github.com>
2023-03-01 09:25:50 +0000

    deploy: geometricpanda/storybook-addon-badges-docs@468cfd0f2c9429eb505c2b5131468b9b7525dd22

--- assets/js/3152febb.e8b34df4.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkstorybook_addon_badges_docs=self.webpackChunkstorybook_addon_badges_docs||[]).push([[225],{3492:t=>{t.exports=JSON.parse('{"title":"Getting Started","slug":"/category/getting-started","permalink":"/docs/category/getting-started","navigation":{"next":{"title":"Installation","permalink":"/docs/getting-started/installation"}}}')}}]);
